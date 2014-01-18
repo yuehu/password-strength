ceb4b2a4b88ad4aeff0ad0d0b0deabe4379df37b
Hsiaoming Yang <me@lepture.com>
2014-01-18 13:02:07 +0800

    Add test case

---
+++ test/spec.js
@@ -0,0 +1,37 @@
+describe('valid-password', function() {
+  var valid = require('..');
+
+  function assert(a, b) {
+    if (a !== b) {
+      throw new Error(a + ' not equal ' + b);
+    }
+  }
+
+  it('is too short', function() {
+    assert(valid('foo').hint, 'too short');
+  });
+
+  it('is too simple', function() {
+    assert(valid('asdfg').hint, 'too simple');
+    assert(valid('abcdef').hint, 'too simple');
+  });
+
+  it('is simple world', function() {
+    valid.words = ['hello'];
+    assert(valid('hello').hint, 'simple word');
+  });
+
+  it('is simple but valid', function() {
+    assert(valid('boom').strength, 'simple');
+    assert(valid('boom').valid, true);
+  });
+
+  it('is medium', function() {
+    assert(valid('foobarbaz').strength, 'medium');
+    assert(valid('bar.baz').strength, 'medium');
+  });
+
+  it('is strong', function() {
+    assert(valid('bar.baz.Foo').strength, 'strong');
+  });
+});
